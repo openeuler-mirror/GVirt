7406024e419902076709e340d05b0d2deec2c9ff
lulina <lina.lulina@huawei.com>
2025-12-08 12:01:05 +0000

    xlite: add prefill latency test case
    Signed-off-by: lulina <lina.lulina@huawei.com>

--- xlite/tests/run_perf.sh
+++ xlite/tests/run_perf.sh
@@ -2,8 +2,11 @@ function run_qwen2_32B()
 {
     tp=$1
     bs=$2
-    new_tokens=$3
-    file=$4
+    input_tokens=$3
+    output_tokens=$4
+    max_seq_len=$(($input_tokens + $output_tokens))
+    file=$5
+    dtype=$6
     echo '{
         "vocab_size": 152064,
         "dim": 5120,
@@ -13,19 +16,22 @@ function run_qwen2_32B()
         "n_kv_heads": 8,
         "norm_eps": 1e-05,
         "rope_theta": 1000000.0,
-        "dtype": "float16",
-        "max_seq_len": '${new_tokens}',
+        "dtype": "'${dtype}'",
+        "max_seq_len": '${max_seq_len}',
         "max_batch_size": '${bs}'
     }' > tests/test_config.json
-    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model qwen2 --ckpt-path /mnt/nvme0n1/models/qwen32b/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${new_tokens} | tee tests/${file}.log
+    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model qwen2 --ckpt-path /mnt/nvme0n1/models/qwen32b/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${output_tokens} | tee tests/${file}.log
 }
 
 function run_qwen3_32B()
 {
     tp=$1
     bs=$2
-    new_tokens=$3
-    file=$4
+    input_tokens=$3
+    output_tokens=$4
+    max_seq_len=$(($input_tokens + $output_tokens))
+    file=$5
+    dtype=$6
     echo '{
         "vocab_size": 151936,
         "dim": 5120,
@@ -36,19 +42,22 @@ function run_qwen3_32B()
         "n_kv_heads": 8,
         "norm_eps": 1e-06,
         "rope_theta": 1000000.0,
-        "dtype": "float16",
-        "max_seq_len": '${new_tokens}',
+        "dtype": "'${dtype}'",
+        "max_seq_len": '${max_seq_len}',
         "max_batch_size": '${bs}'
     }' > tests/test_config.json
-    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model qwen3 --ckpt-path /mnt/nvme0n1/models/Qwen3-32B/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${new_tokens} | tee tests/${file}.log
+    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model qwen3 --ckpt-path /mnt/nvme0n1/models/Qwen3-32B/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${output_tokens} | tee tests/${file}.log
 }
 
 function run_llama_7B()
 {
     tp=$1
     bs=$2
-    new_tokens=$3
-    file=$4
+    input_tokens=$3
+    output_tokens=$4
+    max_seq_len=$(($input_tokens + $output_tokens))
+    file=$5
+    dtype=$6
     echo '{
         "vocab_size": 32000,
         "dim": 4096,
@@ -57,19 +66,22 @@ function run_llama_7B()
         "n_heads": 32,
         "n_kv_heads": 32,
         "norm_eps": 1e-05,
-        "dtype": "float16",
-        "max_seq_len": '${new_tokens}',
+        "dtype": "'${dtype}'",
+        "max_seq_len": '${max_seq_len}',
         "max_batch_size": '${bs}'
     }' > tests/test_config.json
-    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model llama --ckpt-path /mnt/nvme0n1/models/Llama-2-7b-chat-hf/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${new_tokens} | tee tests/${file}.log
+    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model llama --ckpt-path /mnt/nvme0n1/models/Llama-2-7b-chat-hf/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${output_tokens} | tee tests/${file}.log
 }
 
 function run_llama_13B()
 {
     tp=$1
     bs=$2
-    new_tokens=$3
-    file=$4
+    input_tokens=$3
+    output_tokens=$4
+    max_seq_len=$(($input_tokens + $output_tokens))
+    file=$5
+    dtype=$6
     echo '{
         "vocab_size": 32000,
         "dim": 5120,
@@ -78,19 +90,22 @@ function run_llama_13B()
         "n_heads": 40,
         "n_kv_heads": 40,
         "norm_eps": 1e-05,
-        "dtype": "float16",
-        "max_seq_len": '${new_tokens}',
+        "dtype": "'${dtype}'",
+        "max_seq_len": '${max_seq_len}',
         "max_batch_size": '${bs}'
     }' > tests/test_config.json
-    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model llama --ckpt-path /mnt/nvme0n1/models/Llama2-Chinese-13b-Chat/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${new_tokens} | tee tests/${file}.log
+    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model llama --ckpt-path /mnt/nvme0n1/models/Llama2-Chinese-13b-Chat/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${output_tokens} | tee tests/${file}.log
 }
 
 function run_llama_34B()
 {
     tp=$1
     bs=$2
-    new_tokens=$3
-    file=$4
+    input_tokens=$3
+    output_tokens=$4
+    max_seq_len=$(($input_tokens + $output_tokens))
+    file=$5
+    dtype=$6
     echo '{
         "vocab_size": 32000,
         "dim": 8192,
@@ -99,19 +114,21 @@ function run_llama_34B()
         "n_heads": 64,
         "n_kv_heads": 8,
         "norm_eps": 1e-05,
-        "dtype": "float16",
-        "max_seq_len": '${new_tokens}',
+        "dtype": "'${dtype}'",
+        "max_seq_len": '${max_seq_len}',
         "max_batch_size": '${bs}'
     }' > tests/test_config.json
-    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model llama --ckpt-path /mnt/nvme0n1/models/codellama34B/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${new_tokens} | tee tests/${file}.log
+    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --model llama --ckpt-path /mnt/nvme0n1/models/codellama34B/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${output_tokens} | tee tests/${file}.log
 }
 
 function run_deepseek()
 {
     tp=$1
     bs=$2
-    new_tokens=$3
-    file=$4
+    input_tokens=$3
+    output_tokens=$4
+    max_seq_len=$(($input_tokens + $output_tokens))
+    file=$5
     # n_layers: 8
     # quantization: experts_int8
     echo '{
@@ -136,20 +153,21 @@ function run_deepseek()
         "v_head_dim": 128,
         "dtype": "bf16",
         "quantization": "experts_int8",
-        "max_seq_len": '${new_tokens}',
+        "max_seq_len": '${max_seq_len}',
         "max_batch_size": '${bs}'
     }' > tests/test_config.json
 
-    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --ckpt-path /mnt/nvme1n1/models/deepseek-R1-expert-int8-8layers-8d/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${new_tokens} | tee tests/${file}.log
+    torchrun --nproc_per_node=${tp} --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 tests/generate.py --ckpt-path /mnt/nvme1n1/models/deepseek-R1-expert-int8-8layers-8d/ --config tests/test_config.json --input-file tests/${file}.json --max-new-tokens ${output_tokens} | tee tests/${file}.log
 }
 
-function test_perf {
+function test_perf_decode {
     model=$1
     tp=$2
     bs=$3
-    new_tokens=$4
+    output_tokens=$4
     backend=$5
-    file=${backend}_${model}_bs${bs}len${new_tokens}
+    dtype=$6
+    file=${backend}_${model}_${dtype}_bs${bs}len${output_tokens}
     echo '[' > tests/${file}.json
     for ((i=1; i<=${bs}; i++)); do
         if [ $i -ne ${bs} ]; then
@@ -166,24 +184,58 @@ function test_perf {
     done
     echo ']' >> tests/${file}.json
 
+    input_tokens=100
     if [[ "${model}" == "qwen2_32B" ]]; then
-        run_qwen2_32B ${tp} ${bs} ${new_tokens} ${file}
+        run_qwen2_32B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
     elif [[ "${model}" == "qwen3_32B" ]]; then
-        run_qwen3_32B ${tp} ${bs} ${new_tokens} ${file}
+        run_qwen3_32B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
     elif [[ "${model}" == "llama_7B" ]]; then
-        run_llama_7B ${tp} ${bs} ${new_tokens} ${file}
+        run_llama_7B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
     elif [[ "${model}" == "llama_13B" ]]; then
-        run_llama_13B ${tp} ${bs} ${new_tokens} ${file}
+        run_llama_13B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
     elif [[ "${model}" == "llama_34B" ]]; then
-        run_llama_34B ${tp} ${bs} ${new_tokens} ${file}
+        run_llama_34B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
     else
-        run_deepseek ${tp} ${bs} ${new_tokens} ${file}
+        run_deepseek ${tp} ${bs} ${input_tokens} ${output_tokens} ${file}
     fi
     perf_stat=$(cat "tests/${file}.log" | grep "tokens\/s @ " | grep "generate" | head -n 1)
     if [[ "${model}" == "deepseek_8layer" ]]; then
-        echo $perf_stat | awk -v bs="$bs" '{print "| '${model}' | TP'${tp}'moeEP'${tp}' | " $2 / bs " | " $14 " | " $11 " | " $8 " |"}' >> ${perf_log_file}
+        echo $perf_stat | awk -v bs="$bs" '{print "| '${model}' | '${dtype}' | TP'${tp}'moeEP'${tp}' | " $2 / bs " | " $14 " | " $11 " | " $8 " |"}' >> ${perf_log_file}
     else
-        echo $perf_stat | awk -v bs="$bs" '{print "| '${model}' | TP'${tp}' | " $2 / bs " | " $14 " | " $11 " | " $8 " |"}' >> ${perf_log_file}
+        echo $perf_stat | awk -v bs="$bs" '{print "| '${model}' | '${dtype}' | TP'${tp}' | " $2 / bs " | " $14 " | " $11 " | " $8 " |"}' >> ${perf_log_file}
+    fi
+    sleep 10
+}
+
+function test_perf_prefill {
+    model=$1
+    tp=$2
+    bs=$3
+    output_tokens=$4
+    backend=$5
+    dtype=$6
+    file=${backend}_${model}_${dtype}_bs${bs}len${output_tokens}
+    cp tests/test_prefill.json tests/${file}.json
+
+    input_tokens=3456
+    if [[ "${model}" == "qwen2_32B" ]]; then
+        run_qwen2_32B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
+    elif [[ "${model}" == "qwen3_32B" ]]; then
+        run_qwen3_32B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
+    elif [[ "${model}" == "llama_7B" ]]; then
+        run_llama_7B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
+    elif [[ "${model}" == "llama_13B" ]]; then
+        run_llama_13B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
+    elif [[ "${model}" == "llama_34B" ]]; then
+        run_llama_34B ${tp} ${bs} ${input_tokens} ${output_tokens} ${file} ${dtype}
+    else
+        run_deepseek ${tp} ${bs} ${input_tokens} ${output_tokens} ${file}
+    fi
+    perf_stat=$(cat "tests/${file}.log" | grep "tokens\/s @ " | grep "generate" | head -n 1)
+    if [[ "${model}" == "deepseek_8layer" ]]; then
+        echo $perf_stat | awk -v bs="$bs" '{print "| '${model}' | '${dtype}' | TP'${tp}'moeEP'${tp}' | " $2 / bs " | " $14 " | " $11 " |"}' >> ${perf_log_file}
+    else
+        echo $perf_stat | awk -v bs="$bs" '{print "| '${model}' | '${dtype}' | TP'${tp}' | " $2 / bs " | " $14 " | " $11 " |"}' >> ${perf_log_file}
     fi
     sleep 10
 }
@@ -195,53 +247,98 @@ backend=xlite
 export FORWARD_BACKEND=${backend}
 
 perf_log_file=tests/${backend}_perf.log
-echo "| 模型 | 并行策略 | 输入输出长度 | batch size | 平均decode时延 | 平均decode吞吐 |" > ${perf_log_file}
-echo "|--|--|--|--|--|--|" >> ${perf_log_file}
+echo "| 模型 | 并行策略 | 并行策略 | 输入输出长度 | batch size | 平均decode时延 | 平均decode吞吐 |" > ${perf_log_file}
+echo "|--|--|--|--|--|--|--|" >> ${perf_log_file}
+
 
 model=llama_7B
 tp=1
-max_new_tokens=1024
+output_tokens=1024
+dtype=float16
 for bs in 1
 do
-    test_perf ${model} ${tp} ${bs} ${max_new_tokens} ${backend}
+    test_perf_decode ${model} ${tp} ${bs} ${output_tokens} ${backend} ${dtype}
 done
 
 model=llama_13B
 tp=4
-max_new_tokens=1024
+output_tokens=1024
+dtype=float16
 for bs in 1 16
 do
-    test_perf ${model} ${tp} ${bs} ${max_new_tokens} ${backend}
+    test_perf_decode ${model} ${tp} ${bs} ${output_tokens} ${backend} ${dtype}
 done
 
 model=llama_34B
 tp=8
-max_new_tokens=1024
+output_tokens=1024
+dtype=bfloat16
 for bs in 1 16
 do
-    test_perf ${model} ${tp} ${bs} ${max_new_tokens} ${backend}
+    test_perf_decode ${model} ${tp} ${bs} ${output_tokens} ${backend} ${dtype}
 done
 
 model=qwen2_32B
 tp=8
-max_new_tokens=1024
+output_tokens=1024
+dtype=bfloat16
 for bs in 1 16
 do
-    test_perf ${model} ${tp} ${bs} ${max_new_tokens} ${backend}
+    test_perf_decode ${model} ${tp} ${bs} ${output_tokens} ${backend} ${dtype}
 done
 
-model=qwen3_32B
-tp=8
-max_new_tokens=1024
-for bs in 1 16
+for dtype in float16 bfloat16
 do
-    test_perf ${model} ${tp} ${bs} ${max_new_tokens} ${backend}
+    model=qwen3_32B
+    tp=8
+    output_tokens=1024
+    for bs in 1 16
+    do
+        test_perf_decode ${model} ${tp} ${bs} ${output_tokens} ${backend} ${dtype}
+    done
 done
 
-model=deepseek_8layer
+#model=deepseek_8layer
+#tp=8
+#output_tokens=1024
+#dtype=bfloat16
+#for bs in 1 16 64 128 192 256
+#do
+#    test_perf_decode ${model} ${tp} ${bs} ${output_tokens} ${backend} ${dtype}
+#done
+
+echo "| 模型 | 并行策略 | 并行策略 | 输入输出长度 | batch size | prefill时延 |" >> ${perf_log_file}
+echo "|--|--|--|--|--|--|" >> ${perf_log_file}
+
+
+model=llama_7B
+tp=1
+dtype=float16
+test_perf_prefill ${model} ${tp} 1 1 ${backend} ${dtype}
+
+model=llama_13B
+tp=4
+dtype=float16
+test_perf_prefill ${model} ${tp} 1 1 ${backend} ${dtype}
+
+model=llama_34B
 tp=8
-max_new_tokens=1024
-for bs in 1 16 64 128 192 256
+dtype=bfloat16
+test_perf_prefill ${model} ${tp} 1 1 ${backend} ${dtype}
+
+model=qwen2_32B
+tp=8
+dtype=bfloat16
+test_perf_prefill ${model} ${tp} 1 1 ${backend} ${dtype}
+
+for dtype in float16 bfloat16
 do
-    test_perf ${model} ${tp} ${bs} ${max_new_tokens} ${backend}
+    model=qwen3_32B
+    tp=8
+    test_perf_prefill ${model} ${tp} 1 1 ${backend} ${dtype}
 done
+
+#model=deepseek_8layer
+#tp=8
+#dtype=bfloat16
+#test_perf_prefill ${model} ${tp} 1 1 ${backend} ${dtype}
