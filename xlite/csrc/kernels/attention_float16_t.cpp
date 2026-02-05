0fdced8687b6ceef5bf954b58c3defd51168412c
lulina <lina.lulina@huawei.com>
2026-02-05 00:49:37 +0000

    xlite: add attention kernel
    Signed-off-by: lulina <lina.lulina@huawei.com>

---
+++ xlite/csrc/kernels/attention_float16_t.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2026. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#include "attention.h"
+
+ATTN_FUNC_DEFINE(float16_t, float16_t);
