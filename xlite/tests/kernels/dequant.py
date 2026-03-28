7f328bd7c5b761aa1cbfc23b14883c74bf3c761d
wangxiaoran <wangxiaoran11@huawei.com>
2026-03-28 20:52:26 +0800

    xlite: fix bugs of dequant operator

--- xlite/tests/kernels/dequant.py
+++ xlite/tests/kernels/dequant.py
@@ -12,28 +12,33 @@ from xlite._C import Runtime, dequant
 
 rt = Runtime(0, 500)
 torch.npu.set_device(0)
+cases = [
+    [8192, 768],
+    [16, 2048]
+]
 
-for has_scale in [False, True]:
-    x = torch.randn(8192, 768, dtype=torch.float16, device="npu:0")
-    y = torch.randn(768, dtype=torch.float, device="npu:0")
-    z = torch.empty(8192, 768, dtype=torch.bfloat16, device="npu:0")
+for m, n in cases:
+    for has_scale in [False, True]:
+        x = torch.randn(m, n, dtype=torch.float16, device="npu:0")
+        scales = torch.randn(m, dtype=torch.float, device="npu:0")
+        z = torch.empty(m, n, dtype=torch.bfloat16, device="npu:0")
 
-    # torch 采用四舍五入，偶数优先的策略
-    if has_scale:
-        standard = (x.to(torch.float) * y).to(torch.bfloat16)
-    else:
-        standard = x.to(torch.float).to(torch.bfloat16)
+        # torch 采用四舍五入，偶数优先的策略
+        if has_scale:
+            standard = (x.to(torch.float) * scales.unsqueeze(-1)).to(torch.bfloat16)
+        else:
+            standard = x.to(torch.float).to(torch.bfloat16)
 
-    torch.npu.synchronize()
-    dequant(rt, x, y, z, has_scale)
-    torch.npu.synchronize()
-    print(f'Dequant torch.float16 to torch.bfloat { "with scale " if has_scale else "" }executed!')
+        torch.npu.synchronize()
+        dequant(rt, x, scales, z, has_scale)
+        torch.npu.synchronize()
+        print(f'Dequant [{m}, {n}] torch.float16 to torch.bfloat { "with scale " if has_scale else "" }executed!')
 
-    try:
-        torch.testing.assert_close(standard, z, atol=1e-5, rtol=1e-3)
-    except AssertionError as e:
-        print(f'{e}')
-        print(f'x: {x}')
-        print(f'y: {y}')
-        print(f'torch_npu: {standard}')
-        print(f'xlite: {z}')
+        try:
+            torch.testing.assert_close(standard, z, atol=1e-5, rtol=1e-3)
+        except AssertionError as e:
+            print(f'{e}')
+            print(f'x: {x}')
+            print(f'y: {scales}')
+            print(f'torch_npu: {standard}')
+            print(f'xlite: {z}')
