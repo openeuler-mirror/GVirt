175b4a6a70bc16e47f954984ce395f79c8cae1be
Sijie Fu <fusijie@huawei.com>
2026-04-14 18:41:03 +0800

    xlite: doc: add docstring and type hints for the C++ extension module with some argument name changes and fixes

--- xlite/xlite/__init__.py
+++ xlite/xlite/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.1.0rc4'
+"""xlite Python package.
+
+This package exposes the native extension module :mod:`xlite._C`, which
+contains runtime, model, and kernel bindings implemented in ``csrc/_C.cpp``.
+
+Supported Python versions: 3.9 through 3.11.
+"""
+
+__version__ = "0.1.0rc4"
